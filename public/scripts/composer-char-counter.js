4c434ceab2c2a980df3d2fd20f79df8158cb4bc2
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-09 00:13:13 +0000

    nav-bar is now fixed at the top. New edits in the header and new-tweet css files have been made

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,12 @@ $(document).ready(() => {
 
   // keeps track of character count and turns counter red if count is over the limit.
   $(".new-tweet").on("keyup", function() {
-    const counter = $(this.children[1][2]);
+    const counter = $(this.children[0][2]);
     let counterValue = 140;
-    let characterLength = this.children[1][0].value.length;
-    
-    this.children[1][2].value = (counterValue - characterLength);
-    if (this.children[1][2].value < 0) {
+    let characterLength = this.children[0][0].value.length;
+
+    this.children[0][2].value = (counterValue - characterLength);
+    if (this.children[0][2].value < 0) {
       counter.css('color', 'red');
     } else {
       counter.css('color', 'white');
