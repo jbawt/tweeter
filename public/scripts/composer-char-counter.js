b07c7ab4e3d948b81a1cc62308896c5f7aa43f1e
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-09-09 00:53:24 +0000

    edited minor syntax errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,15 @@ $(document).ready(() => {
 
   // keeps track of character count and turns counter red if count is over the limit.
   $(".new-tweet").on("keyup", function() {
-    const counter = $(this.children[0][2]);
+    const $counter = $(this.children[0][2]);
     let counterValue = 140;
     let characterLength = this.children[0][0].value.length;
 
     this.children[0][2].value = (counterValue - characterLength);
     if (this.children[0][2].value < 0) {
-      counter.css('color', 'red');
+      $counter.css('color', 'red');
     } else {
-      counter.css('color', 'white');
+      $counter.css('color', 'white');
     }
   });
 });
